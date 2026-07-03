468aadb5c5f182053db1c7980d325a706ba84777
alanbgro <alanbgse19@outlook.com>
2026-07-03 13:54:59 -0600

    Crea primera version de mi taller dev

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Mi taller dev está funcionando 🚀");
+
+const button = document.querySelector(".button");
+
+button.addEventListener("click", () => {
+  console.log("El usuario quiere ver los proyectos");
+});
